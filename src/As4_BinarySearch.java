5c7a189bdff6a2ff55ab1e322069a8de9427961d
pcgc1 <chalutphilip@gmail.com>
2024-10-23 15:23:32 -0600

    b

--- src/As4_BinarySearch.java
+++ src/As4_BinarySearch.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class As4_BinarySearch {
-    public static String[] allAnimals = {"Abalone", "Anemone", "Blue Whale", "Clownfish", "Dolphin", "Geoduck", "Hermit Crab", "Jellyfish", "Kelp", "Shrimp"};
+    public static Scanner input = new Scanner(System.in);
+    public static String[] allAnimals = {"Abalone", "Anemone", "Blue Whale", "Clownfish", "Dugong", "Geoduck", "Hermit Crab", "Jellyfish", "Kelp", "Shrimp"};
     public static String[] allTypes = {"Mollusk", "Invertebrate", "Whale", "Fish", "Whales", "Clam", "Crustacean", "Invertebrate", "Plant", "Crustacean"};
+    public static boolean[] allEndanger = {true, false, true, false, true, false, false, false, true, true};
 
 
     public static void run(){
 
-        System.out.println("Hello world!");
+        for (String names: allAnimals) {
+            System.out.println(names);
+        }
+
+        System.out.println("Enter a sea creature from the list above");
+        String creature = input.nextLine();
+
+
 
     }//run
 
